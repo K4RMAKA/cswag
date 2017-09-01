0d4dd7918b22a8ffb4f9f089675de3d4d09ea2ce
Ding Zhiyi <minerva@DingdeMacBook-Pro.local>
2017-09-02 00:47:24 +0900

    Initial Commit

---
+++ coat-swag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  coat-swag
+//
+//  Created by Ding Zhiyi on 02/09/2017.
+//  Copyright © 2017 Ding Zhiyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
